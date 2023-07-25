6d7ed356d2d6822424f4b1a93232a95490ba5850
euWeaver <102436061+euWeaver@users.noreply.github.com>
2023-07-25 17:31:02 +0300

    Add files via upload

---
+++ srrrtttpp/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+import CheckoutForm from "./CheckoutForm";
+import "./App.css";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe("pk_test_51NSqh2CExa2kOJRbFf4hgqWnsUL8Cf4POuAxLycFtLsGV3ByheNqSXhK7ThCBPTAyMNjDSLX4Ch00wi5YGVdUfJT00IYHL7EeG");
+
+export default function App() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+    variables: {
+      colorPrimary: '#d7be68',
+      colorBackground: '#2e2e2e',
+      colorText: '#ffffff',
+    },
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
